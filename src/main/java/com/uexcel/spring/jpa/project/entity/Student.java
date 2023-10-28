3d273ce85bb2907c084fb4a3507340590a8d48c1
Uexcel2022 <excellenceudoka@gmail.com>
2023-10-28 21:45:54 +0100

    commit

---
+++ src/main/java/com/uexcel/spring/jpa/project/entity/Student.java
@@ -0,0 +1,10 @@
+package com.uexcel.spring.jpa.project.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Student {
+    private Long studentId;
+    private String firstName;
+
+}
