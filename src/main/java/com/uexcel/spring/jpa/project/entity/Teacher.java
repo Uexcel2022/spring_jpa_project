6871bc70a8ad000a1e7c80961b178c6020482630
Uexcel2022 <excellenceudoka@gmail.com>
2023-10-30 23:12:09 +0100

    commit

--- src/main/java/com/uexcel/spring/jpa/project/entity/Teacher.java
+++ src/main/java/com/uexcel/spring/jpa/project/entity/Teacher.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.query.sqm.FetchClauseType;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,6 +19,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
